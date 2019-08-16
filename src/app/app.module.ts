3d02c8027cd1a5687080b43ffae1d3d498671b8b
Rafael barboza <barboza.rafael.p@gmail.com>
2019-08-16 16:14:23 -0500

    init commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BaseComponent } from './components/widgets/base/base.component';
+import { SliderComponent } from './components/widgets/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BaseComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
